8c365b4d3f40553de29dfbaec768c21350884c62
iamaneeshjk <aneesh.jk@waybeo.com>
2021-04-28 23:17:23 +0530

    initial development

--- src/App.vue
+++ src/App.vue
@@ -1,32 +1,25 @@
 <template>
-  <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
-    </div>
-    <router-view/>
-  </div>
+  <v-app>
+    <router-view />
+   <!--  <chooseNumber /> -->
+    
+  </v-app>
 </template>
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
+<script>
+// import login from './components/login.vue';
+// import chooseNumber from './components/choose_no.vue';
 
-#nav {
-  padding: 30px;
-}
+export default {
+  name: 'App',
 
-#nav a {
-  font-weight: bold;
-  color: #2c3e50;
-}
+  components: {
+    // login,
+    // chooseNumber
+  },
 
-#nav a.router-link-exact-active {
-  color: #42b983;
-}
-</style>
+  data: () => ({
+    //
+  }),
+};
+</script>
--- src/main.js
+++ src/main.js
@@ -1,10 +1,33 @@
 import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
+import vuetify from './plugins/vuetify';
+import firebase from "firebase/app";
+import 'firebase/firestore';
+import axios from 'axios'
+import VueAxios from 'vue-axios'
+import AxiosPlugin from 'vue-axios-cors'
+
 
 Vue.config.productionTip = false
+Vue.use(VueAxios, axios)
+Vue.use(AxiosPlugin)
+// Vue.http.headers.common['Access-Control-Allow-Origin'] = '*'
+firebase.initializeApp({
+	apiKey: "AIzaSyAbKeIOiuumM2AYsvPgJuyytH0CvIwxhsU",
+    authDomain: "tringpartner-v2.firebaseapp.com",
+    databaseURL: "https://tringpartner-v2-default-rtdb.firebaseio.com",
+    projectId: "tringpartner-v2",
+    storageBucket: "tringpartner-v2.appspot.com",
+    messagingSenderId: "448716065880",
+    appId: "1:448716065880:web:fa9c09fe22e88d873b4e2d",
+    measurementId: "G-Y2513C98CT"
+  });
+
+export const db = firebase.firestore();
 
 new Vue({
   router,
+  vuetify,
   render: h => h(App)
 }).$mount('#app')
