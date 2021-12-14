952eef650e3b96996bdc80cab1c44a9247238ef9
iamaneeshjk <aneesh.jk@waybeo.com>
2021-12-14 18:29:17 +0530

    dashboard bug fix

---
+++ src/components/login.vue
@@ -0,0 +1,333 @@
+<template>
+	<v-app class="grey lighten-3">
+	<div class="grey lighten-3" >
+		<v-container >
+		<!-- 	<v-layout > -->
+				<v-row justify="center">
+					<v-col sm="12" lg="9"  class="mx-sm-auto my-md-16">
+						<v-card>
+							<v-overlay :value="overlay">
+								<v-progress-circular indeterminate color="red" size="60" :width="6"></v-progress-circular>
+							</v-overlay>
+							<v-row no-gutters >
+								<v-col  sm="12" md="12" lg="6" justify="center">
+									<div class="mt-3 ml-3">
+										<v-app-bar color="white" flat>
+											
+											<v-spacer></v-spacer>
+											<v-btn icon>
+												<v-icon color='red text-subtitle-2' >mdi-chat-outline</v-icon>
+											</v-btn>
+											<label class='red--text text-subtitle-2'>CHAT WITH US</label>
+										</v-app-bar>
+									</div>
+									<div class="ml-4">
+										<h4 class="mt-6 ml-5">Login or Sign Up</h4>
+										<v-container>
+											<div class="mt-1 ml-2" v-if="getNumber">
+												<v-form @submit.prevent="submit">
+													<!-- <p>{{ phno }} </p> -->
+													<v-text-field v-model='phNo' prefix="+91" color='grey darken-4' label="Enter your Phone Number" type='text' :rules="numberRules" required ></v-text-field>
+													<v-btn class="mr-4 white--text" width="100%" @click.prevent='sendOtp()' color='light-blue darken-1
+													'> REQUEST OTP </v-btn>
+													<!-- <v-btn class="mr-4 white--text" width="100%" @click.prevent='testing()' color='light-blue darken-1
+													'> testing </v-btn> -->
+												</v-form>
+											</div>
+											<!-- <div id="recaptcha-container"></div><br> -->
+											<div class="mt-1 ml-2" v-if="getOtp">
+												<v-form @submit.prevent="submit">
+													<v-text-field v-model='otp' label="Enter OTP" type='text' required ></v-text-field>
+													<v-btn class="mr-4" @click.prevent='sendOtp()' color='white lighten-3' width="40%"> RESENT OTP</v-btn>
+													<v-btn class="mr-4 white--text" @click.prevent='verifyOtp()' color='light-blue darken-1
+													' width="40%"> LOGIN </v-btn>
+												</v-form>
+											</div>
+										</v-container>
+										<p class="mt-1 ml-5 font-weight-regular">For verification, an sms will be be sent to your mobile number. By proceeding, you agree to our <a href="https://www.tringpartner.com/terms" target="_blank"  class="text-blue">Terms & Conditions</a> & <a href="https://www.tringpartner.com/privacy" target="_blank" class="text-blue">Privacy Policy. </a>
+										</p>
+										<div>
+											<p class="ml-5 mt-4 grey--text font-weight-medium">Trusted by</p>
+											<v-flex class="d-flex justify-space-around mb-5">
+
+											</v-flex>
+										</div>
+									</div>
+								</v-col>
+								<v-col sm="12" md="12" lg="6">
+									<div class="video-wrapper mt-3 ml-10  ml-md-9 ">	
+									</div>
+								</v-col>
+							</v-row>
+							<div id="recaptcha-container"></div>
+						</v-card>
+					</v-col>
+				</v-row>
+      <v-dialog
+        v-model="dialog"
+        transition="dialog-bottom-transition"
+        max-width="400"
+      >
+        <template v-slot:default="dialog">
+          <v-card outlined shaped elevation="8">
+            <v-card-text>
+              <div class="text-h6 mt-4 red--text">Oops something went wrong</div>
+              <div class="text-h6 mt-2 red--text">Try after sometime !! </div>
+            </v-card-text>
+            <v-card-actions class="justify-end">
+              <v-btn
+                text
+                color="danger"
+                @click="dialog.value = false"
+              >Close</v-btn>
+            </v-card-actions>
+          </v-card>
+        </template>
+      </v-dialog>
+		</v-container>
+  </div>
+</v-app>
+</template>
+
+<script>
+import firebase from 'firebase'
+import { db } from '@/main.js';
+	export default {
+	data(){
+		return{
+		appVerifier : '',
+		otp : '',
+		getNumber : true,
+		getOtp : false,
+		phNo : '',
+		uid : '',
+		currentPage: '',
+		dialog : false,
+		errorMessage: '',
+		overlay : false,
+		role : '',
+		numberRules: [ 
+			// v => (v && v.length == 10 ||  'Number must contain 10 digits. Invalid Number !!'),
+			v => /^[0-9]*$/.test(v) || 'No characters Allowed',
+			],
+		}
+	},
+	methods: {
+			sendOtp(){
+				if(this.phNo.length != 10){
+					alert('Invalid Phone Number Format !');
+				} else {
+					this.getNumber = false
+					this.getOtp = true
+					let countryCode = '+91'
+					let phoneNumber = countryCode + this.phNo
+					let appVerifier = this.appVerifier
+					firebase.auth().signInWithPhoneNumber(phoneNumber, appVerifier)
+						.then((confirmationResult) => {
+							window.confirmationResult = confirmationResult;
+						}).catch((error) => {
+							this.errorMessage = error.message
+							console.log(this.errorMessage)
+							// this.$analytics.logEvent("Web Otp send");
+							// this.sendErrorLog(errorMessage)
+						})
+				}
+			},
+	verifyOtp(){
+		if(this.otp.length != 6){
+			alert('Invalid OTP Format !');
+		}	else {
+			this.overlay = true
+			let userid = ''
+			let code = this.otp
+			window.confirmationResult.confirm(code).then(function (result) {
+			var user = result.user;
+			userid = result.user.uid;
+			console.log('user details', user)
+			console.log('user details', userid)
+			// this.uid = this.userid
+			}).catch(function (error) {
+			console.log('error details', error)
+			// alert('Invalid OTP')
+			}).finally(() => {
+						this.uid = userid
+						console.log('ID', this.uid)
+						console.log('phno', this.phNo)
+						const options = {
+							url: 'https://asia-south1-test-tpv2.cloudfunctions.net/tpv2/login',
+							method: 'POST',
+							data: {
+								uid: this.uid,
+								phoneNumber: this.phNo
+							}
+						}
+						console.log(options)
+						this.$axios(options)
+							.then((response) => {
+								console.log(response.data)
+								console.log(response.data.token)
+								localStorage.setItem('token',response.data.token)
+								db.collection('users').where("uid" , "==" , this.uid).get().then((querySnapshot) => {
+									querySnapshot.forEach((doc) => {
+										console.log(doc.id, " => ", doc.data());
+										let user_details = doc.data()
+										this.Udata = user_details
+										this.currentPage = this.Udata.currentPage
+										this.role = this.Udata.role
+										console.log(this.currentPage)
+										console.log(this.role)
+										this.$analytics.logEvent("Web Otp verified");
+										if(this.role == 'ADMIN' || this.role == 'AGENT') {
+											this.overlay = false
+											this.$router.push("/all_calls")
+										}
+										else {
+											if (this.currentPage == "onboarding_listing") {
+												this.overlay = false
+												this.$router.push("/choose_no")
+											}
+											else if (this.currentPage == "onboarding_test_completed") {
+												this.overlay = false
+												this.$router.push("/test_number")
+											}
+											else if (this.currentPage == "onboarding_plan_details") {
+												this.overlay = false
+												this.$router.push("/pricing")
+											}
+											else if (this.currentPage == "onboarding_billing") {
+												this.overlay = false
+												this.$router.push("/all_calls")
+											}
+											else if (this.currentPage == "onboarding_success") {
+												this.overlay = false
+												this.$router.push("/emailVerification")
+											}
+											else if (this.currentPage == "onboarding_dashboard") {
+												// this.overlay = false
+												this.$router.push("/all_calls")
+											}
+											else {
+												const user_stage = {
+												url: 'https://asia-south1-test-tpv2.cloudfunctions.net/tpv2/user/stage',
+												method: 'POST',
+												data: {
+															uid: this.uid,
+															phoneNumber: this.phno,
+															currentPage: 'onboarding_listing'
+												},
+											}
+											console.log(user_stage)
+											this.$axios(user_stage)
+												.then((response) => {
+													console.log(response)
+													this.overlay = false
+													this.$analytics.logEvent("Web onboarding_listing");
+													this.$router.push("/choose_no")
+												})
+												.catch((error) => {
+													console.error(error);
+												})
+											}
+										}
+										})
+										}).catch((error) => {
+											console.log("Error getting documents: ", error);
+										})
+							})
+							.catch((error) => {
+								console.error(error);
+							})
+						// this.writeDb() 
+			});
+				}
+		},
+			initReCaptcha(){
+		setTimeout(()=>{
+			let vm = this
+			console.log('render on create',vm)
+			window.recaptchaVerifier = new firebase.auth.RecaptchaVerifier('recaptcha-container', {
+			'size': 'invisible',
+			'callback': function(response) {
+				console.log('reCAPTCHA', response)
+			},
+			'expired-callback': function() {
+			}
+			});
+			this.appVerifier =  window.recaptchaVerifier
+		},1000)
+		},
+		sendErrorLog(errorMessage){
+			this.dialog = true
+			const errorLog = {
+				url: 'https://asia-south1-test-tpv2.cloudfunctions.net/tpv2/login/failure',
+				method: 'POST',
+				data: {
+					reason: errorMessage,
+					login_number : this.phNo
+				},
+			}
+			console.log(errorLog)
+				.$axios(errorLog)
+				.then((response) => {
+					console.log(response)
+					this.dialog =  true
+				})
+				.catch((error) => {
+					console.error(error);
+				})
+		}
+		},
+		created() {
+			this.initReCaptcha()	
+			firebase.auth().onAuthStateChanged(user => {
+				if (user) {
+					console.log("logged user details",user)
+					this.uid = user.uid
+					this.phno = user.phoneNumber.slice(3)
+					console.log("user id", this.uid)
+					console.log("user number", this.phno)
+					db.collection('users').where("uid" , "==" , this.uid).get().then((querySnapshot) => {
+						querySnapshot.forEach((doc) => {
+							console.log(doc.id, " => ", doc.data());
+							let user_details = doc.data()
+							this.Udata = user_details
+							this.currentPage = this.Udata.currentPage
+							console.log(this.currentPage)
+							this.role = this.Udata.role
+							console.log(this.currentPage)
+							console.log(this.role)
+							if(this.role == 'ADMIN' || this.role == 'AGENT') {
+								this.$router.push("/all_calls")
+							}
+							else {
+								if (this.currentPage == "onboarding_listing") {
+									this.$router.push("/choose_no")
+								}
+								else if (this.currentPage == "onboarding_test_completed") {
+									this.$router.push("/test_number")
+								}
+								else if (this.currentPage == "onboarding_plan_details") {
+									this.$router.push("/pricing")
+								}
+								else if (this.currentPage == "onboarding_billing") {
+									this.$router.push("/billing")
+								}
+								else if (this.currentPage == "onboarding_success") {
+									this.$router.push("/emailVerification")
+								}
+								else if (this.currentPage == "onboarding_dashboard") {
+									this.$router.push("/all_calls")
+								}
+							}
+						})
+					}).catch((error) => {
+						console.log("Error getting documents: ", error);
+					})
+				}
+			})
+			let url = new URL(location.href).searchParams.get('number')
+			this.phNo = url
+		}
+	}
+	
+</script>
---
+++ src/views/Dashboard.vue
@@ -0,0 +1,182 @@
+<template>
+	<v-app >
+		<div>
+			<v-container  fluid>
+				<v-layout >
+					<v-flex xs12 sm12 md12>
+							<v-row no-gutters>
+								<v-col cols="12">
+									<div class="ml-8">
+										<v-row>
+											<v-col cols="12" sm="10">
+												<h2 class="page_title mt-6 mb-5">Dashboard</h2>
+											</v-col>
+											<v-col cols="12" sm="2" align="end">
+												<span ><v-icon  class="mt-6 mb-5 mr-7" color="black" >mdi-menu</v-icon> </span>
+											</v-col>
+										</v-row>
+										<v-row>
+											<v-col cols="6">
+												<h4 class="heading mt-4 mb-5"> 
+													<Icon class="mr-3" :inline="true" color="red" icon="mdi:call-missed" width="24" height="24">	</Icon>
+														Today's Missed Calls
+												</h4>
+												<v-expansion-panels accordion flat  v-model="missedCallPanel" multiple>
+													<v-expansion-panel v-for="(item,i) in 2" :key="i" >
+														<v-expansion-panel-header >
+															<div>
+																<v-row no-gutters>
+																	<v-col cols="2" flex >
+																		<h4 class="name_heading font-weight-light mt-2"> Shinu (2)</h4>
+																		<br>
+																	</v-col>
+																	<v-spacer></v-spacer>
+																	<v-col cols="10" flex>
+																		<v-divider class="mt-4"></v-divider>
+																	</v-col>
+																</v-row>
+															</div>
+														</v-expansion-panel-header>
+														<v-expansion-panel-content v-for="(item,i) in 2" :key="i">
+															<v-row>
+																<v-col cols="12" sm="10">
+																	<h3  class="number_heading font-weight-light">+91 88919 78 085</h3>
+																	<h6 class="comment_heading font-weight-light">00.10 , 3 mins ago, Shinu</h6> 
+																	<br>
+																</v-col>
+																<v-spacer></v-spacer>
+																<v-col cols="12" sm="2" align="end">
+																	<v-menu offset-y>
+																		<template v-slot:activator="{ on, attrs }">
+																			<v-icon v-bind="attrs" v-on="on" color="black" >mdi-dots-vertical</v-icon>
+																		</template>
+																		<v-list>
+																			<v-list-item v-for="(item, index) in items" :key="index">
+																				<v-list-item-title :class="item.color">{{ item.title }}</v-list-item-title>
+																			</v-list-item>
+																		</v-list>
+																	</v-menu>
+
+																</v-col>
+															</v-row>
+														</v-expansion-panel-content>
+													</v-expansion-panel>
+												</v-expansion-panels>
+											</v-col>
+											<v-col cols="6" align="center">
+												<h4 class="heading"><v-icon class="mt-4 mb-5 mr-3" color="black">mdi-alarm</v-icon> Upcoming Reminders </h4>
+												<v-row>
+													<v-col cols="12" align="center">
+														<div v-for="(item,i) in 2" :key="i" class="mb-3 mt-5">
+															<h4 class="number_heading font-weight-light mr-15">+91 88919 78 085</h4>
+															<div class="mr-16">
+																<h5 class="comment_heading font-weight-light mr-16 mt-1" >04:15 pm</h5>
+															</div>
+														</div>
+													</v-col>
+												</v-row>
+											</v-col>
+										</v-row>
+										<v-row>
+											<v-col cols="6">
+												<h4 class="heading  mb-5"> <v-icon color="black" class="mr-3">mdi-call-split</v-icon> Team Skipped Calls</h4>
+												<v-expansion-panels accordion flat v-model="skippedCallPanel" multiple>
+													<v-expansion-panel v-for="(item,i) in 2" :key="i">
+														<v-expansion-panel-header >
+															<div>
+																<v-row no-gutters>
+																	<v-col cols="2" flex >
+																		<h4  class="name_heading font-weight-light mt-2"> Shinu (2)</h4>
+																		<br>
+																	</v-col>
+																	<v-spacer></v-spacer>
+																	<v-col cols="10" flex>
+																		<v-divider class="mt-4"></v-divider>
+																	</v-col>
+																</v-row>
+															</div>
+														</v-expansion-panel-header>
+														<v-expansion-panel-content v-for="(item,i) in 2" :key="i">
+															<v-row>
+																<v-col cols="12" sm="10">
+																	<h3  class="number_heading font-weight-light">+91 88919 78 085</h3>
+																	<h6 class="comment_heading font-weight-light">00.10 , 3 mins ago, Shinu</h6>
+																	<br>
+																</v-col>
+																<v-spacer></v-spacer>
+																<v-col cols="12" sm="2" align="end">
+																	<v-menu offset-y>
+																		<template v-slot:activator="{ on, attrs }">
+																			<v-icon v-bind="attrs" v-on="on" color="black" >mdi-dots-vertical</v-icon>
+																		</template>
+																		<v-list>
+																			<v-list-item v-for="(item, index) in items" :key="index">
+																				<v-list-item-title :class="item.color">{{ item.title }}</v-list-item-title>
+																			</v-list-item>
+																		</v-list>
+																	</v-menu>
+
+																</v-col>
+															</v-row>
+														</v-expansion-panel-content>
+													</v-expansion-panel>
+												</v-expansion-panels>
+											</v-col>
+										</v-row>
+									</div>
+								</v-col>
+							</v-row>
+
+					</v-flex>
+				</v-layout>
+			</v-container>
+		</div>
+	</v-app>
+</template>
+
+<script>
+	import { Icon } from '@iconify/vue2';
+  export default {
+
+		components: {
+			Icon,
+		},
+		created() {
+
+    },
+    data: () => ({
+			missedCallPanel: [0, 1],
+			skippedCallPanel: [0, 1],
+    }),
+
+    methods:{
+
+    }
+  }
+  </script>
+
+  <style scoped>
+.page_title {
+	font-family: 'Lato', Bold;
+  font-size: 23px 
+}
+.heading {
+	font-family: 'lato', Bold;
+  font-size: 16px 
+}
+.name_heading {
+	font-family: 'Nunito', bold;
+  font-size: 14px 	
+}
+.number_heading {
+	font-family: 'Nunito', bold;
+  font-size: 16px; 	
+  color: #3B3B3B;
+}
+.comment_heading {
+	font-family: 'Nunito', Light;
+  font-size: 10px;
+  color: #808080;
+}
+
+</style>
