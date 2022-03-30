f14a59fc5a9dd8ec8f39c8a699e8881038a5d7c3
jaison john <jaison.john@waybeo.com>
2022-03-30 23:55:47 +0530

    global snackbar component for global use

--- src/App.vue
+++ src/App.vue
@@ -4,16 +4,21 @@
 
     <v-main class="ma-4">
     </v-main>
-  
+    <vtoast ref="vtoast"/>
   </v-app>
 </template>
 
 <script>
 import Navbar from '@/components/Navbar.vue';
+import vtoast from '@/components/snackbar.vue';
 export default {
   name: 'App',
   components: {
   Navbar,
+  vtoast 
   },
+  mounted() {
+    this.$root.vtoast = this.$refs.vtoast;
+  }
 };
 </script>
