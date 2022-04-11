ab82e5bb18358a7640493a337b56e5c5a6b2773d
vetrivelvetri <vetrivelavan.sv@waybeo.com>
2022-04-11 09:12:01 +0530

    addon users

--- src/views/manageAddonUsers.vue
+++ src/views/manageAddonUsers.vue
@@ -47,6 +47,7 @@
                     outlined
                     class="mt-5"
                     max-width="1069"
+                      v-for="(user) in users" :key="user.id"
                   >
                     <v-layout>
                       <v-flex xs12 sm12 md12>
@@ -63,10 +64,10 @@
                                           color="transparent"
                                           class="mb-3"
                                         >
-                                          <div class="agent_name">Sree</div>
-                                          <div class="agent_role">Admin</div>
+                                          <div class="agent_name">{{user.Name}}</div>
+                                          <div class="agent_role">{{user.Role}}</div>
                                           <div class="agent_number">
-                                            +91 989999 9900
+                                            {{user.PhoneNumber}}
                                           </div>
                                         </v-card>
                                       </v-col>
@@ -84,112 +85,14 @@
                                           </template>
                                           <v-list>
                                             <v-list-item
-                                              v-for="(item, index) in options"
+                                              v-for="(item, index) in user.options"
                                               :key="index"
                                               active-class="pink--text"
                                             >
                                               <v-list-item-title
                                                 :class="item.color"
-                                                @click="showPopup(item.url)"
+                                                @click="option_click(item.function,user.PhoneNumber,user.Role,user.Name,user.Uid)"
                                                 :id="item.url"
-                                                >{{
-                                                  item.title
-                                                }}</v-list-item-title
-                                              >
-                                            </v-list-item>
-                                          </v-list>
-                                        </v-menu>
-                                      </v-col>
-                                    </v-row>
-                                    <v-divider></v-divider>
-                                  </v-col>
-                                </v-row>
-                                <v-row>
-                                  <v-col cols="12" sm="12" align="center">
-                                    <v-row justify="space-between">
-                                      <v-col cols="6" sm="6" align="left">
-                                        <v-card
-                                          outlined
-                                          color="transparent"
-                                          class="mb-3"
-                                        >
-                                          <div class="agent_name">Shinu</div>
-                                          <div class="agent_role">Agent</div>
-                                          <div class="agent_number">
-                                            +91 989999 9900
-                                          </div>
-                                        </v-card>
-                                      </v-col>
-                                      <v-col cols="6" sm="2" align="end">
-                                        <v-menu offset-y>
-                                          <template
-                                            v-slot:activator="{ on, attrs }"
-                                          >
-                                            <v-icon
-                                              v-bind="attrs"
-                                              v-on="on"
-                                              color="black"
-                                              >mdi-dots-vertical</v-icon
-                                            >
-                                          </template>
-                                          <v-list>
-                                            <v-list-item
-                                              v-for="(item, index) in options"
-                                              :key="index"
-                                              active-class="pink--text"
-                                            >
-                                              <v-list-item-title
-                                                :class="item.color"
-                                                @click="showPopup(item.url)"
-                                                >{{
-                                                  item.title
-                                                }}</v-list-item-title
-                                              >
-                                            </v-list-item>
-                                          </v-list>
-                                        </v-menu>
-                                      </v-col>
-                                    </v-row>
-                                    <v-divider></v-divider>
-                                  </v-col>
-                                </v-row>
-                                <v-row>
-                                  <v-col cols="12" sm="12" align="center">
-                                    <v-row justify="space-between">
-                                      <v-col cols="6" sm="6" align="left">
-                                        <v-card
-                                          outlined
-                                          color="transparent"
-                                          class="mb-3"
-                                        >
-                                          <div class="agent_name">Shinu</div>
-                                          <div class="agent_role">Agent</div>
-                                          <div class="agent_number">
-                                            +91 989999 9900
-                                          </div>
-                                        </v-card>
-                                      </v-col>
-                                      <v-col cols="6" sm="2" align="end">
-                                        <v-menu offset-y>
-                                          <template
-                                            v-slot:activator="{ on, attrs }"
-                                          >
-                                            <v-icon
-                                              v-bind="attrs"
-                                              v-on="on"
-                                              color="black"
-                                              >mdi-dots-vertical</v-icon
-                                            >
-                                          </template>
-                                          <v-list>
-                                            <v-list-item
-                                              v-for="(item, index) in options"
-                                              :key="index"
-                                              active-class="pink--text"
-                                            >
-                                              <v-list-item-title
-                                                :class="item.color"
-                                                @click="showPopup(item.url)"
                                                 >{{
                                                   item.title
                                                 }}</v-list-item-title
@@ -220,13 +123,12 @@
     <v-dialog v-model="dialog2" max-width="332px">
       <v-card class="rounded-lg pt-7 pb-7">
         <v-card-title class="d-flex justify-center">
-          <h3 class="center">Add New User</h3>
+          <h3 class="center">{{title}}</h3>
         </v-card-title>
         <v-card-text class="pt-0">
-          <v-text-field label="Name" outlined></v-text-field>
-          <v-select :items="types" label="Role" outlined></v-select>
-
-          <v-text-field label="Mobile Number*" outlined></v-text-field>
+           <v-text-field label="Name" outlined v-model="selected_name"></v-text-field>
+          <v-select :items="types" label="Role" outlined v-model="selected_role"></v-select>
+          <v-text-field label="Mobile Number*" outlined v-model="selected_number"></v-text-field>
         </v-card-text>
         <v-card-actions>
           <v-btn
@@ -234,7 +136,7 @@
             text
             class="ma-2 text-capitalize rounded-pill p-3 red_button_outline"
             min-width="140px"
-            @click="dialog2 = false"
+            @click="close()"
           >
             Cancel
           </v-btn>
@@ -244,6 +146,40 @@
             min-width="140px"
             color="white"
             outlined
+            @click="add_user1()"
+          >
+            Submit
+          </v-btn>
+        </v-card-actions>
+      </v-card>
+    </v-dialog>
+     <v-dialog v-model="edit_dialog" max-width="332px">
+      <v-card class="rounded-lg pt-7 pb-7">
+        <v-card-title class="d-flex justify-center">
+          <h3 class="center">{{title}}</h3>
+        </v-card-title>
+        <v-card-text class="pt-0">
+          <v-text-field label="Name" outlined v-model="selected_name"></v-text-field>
+          <v-select :items="types" label="Role" outlined v-model="selected_role"></v-select>
+          <v-text-field label="Mobile Number*" outlined v-model="selected_number"></v-text-field>
+        </v-card-text>
+        <v-card-actions>
+          <v-btn
+            color="red"
+            text
+            class="ma-2 text-capitalize rounded-pill p-3 red_button_outline"
+            min-width="140px"
+            @click="close()"
+          >
+            Cancel
+          </v-btn>
+          <v-btn
+            text
+            class="text-capitalize ma-3 rounded-pill red_button"
+            min-width="140px"
+            color="white"
+            outlined
+            @click="edit_user1()"
           >
             Submit
           </v-btn>
@@ -268,7 +204,7 @@
             text
             class="ma-2 text-capitalize rounded-pill p-3 red_button_outline"
             min-width="140px"
-            @click="removeNumber = false"
+            @click="close()"
           >
             Cancel
           </v-btn>
@@ -289,33 +225,26 @@
 
 <script>
 import { db } from "@/main.js";
+import axios from "axios";
 export default {
   
-  components: {},
-  created() {
-    let localStorageUserObj = JSON.parse(localStorage.getItem("tpu"));
-		const owneruid = (localStorageUserObj.role == "OWNER") ? localStorageUserObj.uid : localStorageUserObj.OwnerUid;
-    // console.log(owneruid)
-    this.owneruid = owneruid;
-    this.uid = localStorageUserObj.uid;
-		// console.log("vetri",owneruid)
-      db.collection("users").where("uid","==",owneruid).get().then(async(snap) =>{
-			// console.log("test.........",snap.docs.data());
-      this.baseusers = snap.docs[0].data().PlanBaseUsers;
-      this.totalusers = snap.docs[0].data().PlanNumberOfUsers;
-			snap.docs.forEach((element)=> {
-				this.users.push({Name:element.data().FirstName,role:element.data().role,PhoneNumber:element.data().PhoneNumber,cron:true,option:[{title:"Edit Title",type:"Edit",headline:"Edit User"}]});
-			});
-    
-		}).catch((err)=>{
-			console.log(err.message)
-		})
+ components: {},
+ created() {
+  this.inital_data()
   },
   data: () => ({
+    selected_name:"",
+    selected_role:"",
+    selected_number:"",
+    selected_uid:"",
+    title:"",
+    users:[],
     uid:"",
     owneruid:"",
+    AccountId:"",
     dialog2: false,
     dialog: false,
+    edit_dialog:false,
     removeNumber: false,
     isActive: true,
     e2: 1,
@@ -330,10 +259,10 @@ export default {
     stepForm: [],
     types: [
       {
-        text: "Agent",
+        text: "ADMIN",
       },
       {
-        text: "Admin",
+        text: "AGENT",
       },
     ],
     items: [
@@ -363,14 +292,144 @@ export default {
     },
   },
   methods: {
-    showPopup(type) {
-      if (type == "Edit") {
-        this.dialog2 = true;
-      }
-      if (type == "removeSlot") {
-        this.removeNumber = true;
-      }
+    option_click(clicked,PhoneNumber,Role,Name,Uid){
+      this[clicked](PhoneNumber,Role,Name,Uid)
+      // this.dialog2=true;
     },
+    add_user(){
+     this.title = "Add User"
+     this.dialog2=true;
+     this.selected_name = "",
+    this.selected_role="",
+    this.selected_number="",
+    this.selected_uid=""
+    },
+    edit_user(PhoneNumber,Role,Name,Uid){
+      this.title = "Edit User"
+      this.edit_dialog=true;
+      this.selected_name = Name,
+    this.selected_role=Role,
+    this.selected_number=PhoneNumber,
+    this.selected_uid=Uid
+    },
+    // submit_click(clicked){
+    //  this[clicked]()
+    // },
+    add_user1(){
+       console.log(this.selected_name,this.selected_role,this.selected_number,this.selected_uid)
+      console.log("nsfjk")
+      const details = {
+        url: "https://asia-south1-test-tpv2.cloudfunctions.net/tpv2/addon/user",
+        method: "POST",
+        headers: { token: localStorage.getItem("token") },
+        data: {
+          updated_by: this.uid,
+          created_by: this.uid,
+          owner_uid: this.owneruid,
+          IsAddon:true,
+          AccountId:this.AccountId,
+          user_number: parseInt(this.selected_number),
+          role: this.selected_role,
+          user_name:this.selected_name
+        },
+      };
+      axios(details).then(async (response) => {
+        console.log(response)
+        this.dialog2 = false;
+         this.$root.vtoast.show({message: 'added successfully', color: 'green', timer: 5000});
+         this.inital_data();
+            
+      })
+    },
+    edit_user1(){
+    console.log(this.selected_name,this.selected_role,this.selected_number,this.selected_uid)
+      console.log(this.selected_name,this.selected_role,this.selected_number,this.selected_uid)
+      console.log("nsfjk")
+      const details = {
+        url: "https://asia-south1-test-tpv2.cloudfunctions.net/tpv2/addon/user",
+        method: "PUT",
+        headers: { token: localStorage.getItem("token") },
+        data: {
+          updated_by: this.uid,
+          created_by: this.uid,
+          owner_uid: this.owneruid,
+          IsAddon:true,
+          AccountId:this.AccountId,
+          user_number: parseInt(this.selected_number),
+          role: this.selected_role,
+          user_name:this.selected_name,
+          user_uid:this.selected_uid
+        },
+      };
+      axios(details).then(async (response) => {
+        console.log(response)
+        this.edit_dialog = false;
+         this.$root.vtoast.show({message: 'added successfully', color: 'green', timer: 5000});
+         this.inital_data();
+            
+      })
+    },
+    delete_slot(){
+     const details = {
+        url: "https://asia-south1-test-tpv2.cloudfunctions.net/tpv2/addon/delete",
+        method: "POST",
+        headers: { token: localStorage.getItem("token") },
+        data: {
+          updated_by: this.uid,
+          owner_uid: this.owneruid,
+          type:"USER",
+        },
+      };
+      axios(details).then(async () => {
+        // console.log(response)
+         this.initial_data();
+         this.$root.vtoast.show({message: 'slot deleted successfully', color: 'green', timer: 5000});
+        //  await this.initial_data();
+        //  this.dialog2 = false         
+      })
+    },
+    delete_user(PhoneNumber){
+     const details = {
+        url: "https://asia-south1-test-tpv2.cloudfunctions.net/tpv2/addon/user",
+        method: "DELETE",
+        headers: { token: localStorage.getItem("token") },
+        data: {
+          updated_by: this.uid,
+          created_by: this.uid,
+          owner_uid: this.owneruid,
+          IsAddon:true,
+          AccountId:this.AccountId,
+          user_number: parseInt(PhoneNumber)
+        },
+      };
+      axios(details).then(async (response) => {
+        console.log(response)
+        // this.edit_dialog = false;
+        if(response.data.status == true){
+         this.$root.vtoast.show({message: 'deleted successfully', color: 'green', timer: 5000});
+          this.inital_data();
+        }
+         if(response.data.status == false){
+         this.$root.vtoast.show({message: response.data.message, color: 'red', timer: 5000});
+        }        
+      })
+    },
+    close(){
+      this.dialog2 = false;
+       this.edit_dialog=false;
+       this.selected_name="";
+    this.selected_role="";
+    this.selected_number="";
+    this.selected_uid="";
+    },
+    // showPopup(type) {
+    //   if (type == "Edit") {
+    //     this.dialog2 = true;
+    //   }
+    //   if (type == "removeSlot") {
+    //     this.removeNumber = true;
+    //   }
+    // },
     CallFlowSettings() {
       this.$router.push("/CallFlowSettings");
     },
@@ -392,6 +451,44 @@ export default {
     done() {
       this.curr = 5;
     },
+    inital_data(){
+        let localStorageUserObj = JSON.parse(localStorage.getItem("tpu"));
+		const owneruid = (localStorageUserObj.role == "OWNER") ? localStorageUserObj.uid : localStorageUserObj.OwnerUid;
+    this.AccountId = (localStorageUserObj.role == "OWNER") ? localStorageUserObj.AccountId : localStorageUserObj.OwnerAccountId;
+    // console.log(owneruid)
+    this.owneruid = owneruid;
+    this.uid = localStorageUserObj.uid;
+		// console.log("vetri",owneruid)
+      this.users=[];
+      db.collection("users").where("OwnerUid","==",owneruid).where("IsAddon","==",true).get().then(async(snap) =>{
+		
+      // snap.docs.forEach(async(element,index) => {
+        for(let [index,element] of snap.docs.entries()){
+               this.users.push({Name:element.data().Name,Role:element.data().role,PhoneNumber:element.data().PhoneNumber,Uid:element.data().uid,options:[{title:"Edit User",type:"Edit",headline:"Edit User",function:"edit_user"},{title:"Delete User",type:"Edit",headline:"Edit User",function:"delete_user"}]});
+               console.log(this.users[index])
+        }
+      // this.baseusers = snap.docs[0].data().PlanBaseUsers;
+      // const totalusers = snap.docs[0].data().PlanNumberOfUsers-snap.docs[0].data().PlanBaseUsers;
+      //  for(let i=0;i<totalusers;i++){ this.users.push({Name:"unassigned",cron:true,options:[{title:"Add User",type:"Edit",headline:"Edit User"},{title:"Delete User",type:"Edit",headline:"Edit User"}]});}
+		// console.log(this.users[1])
+		}).catch((err)=>{
+			console.log(err.message)
+		})
+      db.collection("users").where("uid","==",owneruid).get().then(async(snap) =>{
+			// console.log("test.........",snap.docs.data());
+      // this.baseusers = snap.docs[0].data().PlanBaseUsers;
+      const assignedusers = this.users.length;
+      const totalusers = snap.docs[0].data().PlanNumberOfUsers-snap.docs[0].data().PlanBaseUsers;
+      const toassign = totalusers - assignedusers
+       for(let i=0;i<toassign;i++){ 
+        //  console.log("1")
+         this.users.push({Name:"unassigned",options:[{title:"Add User",type:"Edit",headline:"Edit User",function:"add_user"},{title:"Delete Slot",type:"Edit",headline:"Edit User",function:"delete_slot"}]});
+         }
+		
+		}).catch((err)=>{
+			console.log(err.message)
+		})
+    }
   },
 };
 </script>
